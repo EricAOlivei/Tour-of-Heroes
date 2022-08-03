7448fdcfe20ecfe351e7d18ad8f817db9b256812
Eric Oliveira <oliveiras.eric2012@gmail.com>
2022-08-03 14:02:48 -0300

    Aula 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from './models/hero';
+import { Hero, HEROES } from './models';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +9,15 @@ import { Hero } from './models/hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
+  heroes = HEROES;
+  selectedHero?: Hero;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
